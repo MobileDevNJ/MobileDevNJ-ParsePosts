01143ecf84f4c78b16a139478147a523ca57cb47
David Rodriguez <programmerdave@gmail.com>
2012-04-11 16:28:19 -0400

    Initial Commit

---
+++ ParsePosts/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ParsePosts
+//
+//  Created by David Rodriguez on 4/11/12.
+//  Copyright (c) 2012 Forge42. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ParsePosts/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ParsePosts
+//
+//  Created by David Rodriguez on 4/11/12.
+//  Copyright (c) 2012 Forge42. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
